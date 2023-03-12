793bf60b59d5cb6caf061156e7f906bdf3d07cc2
Mas 'Edi <85278436+masedi54@users.noreply.github.com>
2023-03-12 13:26:48 +0700

    Add files via upload

---
+++ ButtonScript.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour {
+	
+//deklarasi
+
+	public GameObject karakter;
+	public int idButton;
+
+	public float moveSpeed = 0.05f;
+	private Animator anim;
+
+	public bool atas, bawah, kanan, kiri;
+
+	// Use this for initialization
+	void Start () {
+		anim = karakter.GetComponent<Animator>();
+		atas = false;
+		bawah = false;
+		kanan = false;
+		kiri = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (atas){
+			karakter.transform.position = karakter.transform.position 
+											+ new Vector3(0, moveSpeed, 0);
+		}
+		else if (bawah){
+			karakter.transform.position = karakter.transform.position 
+											- new Vector3(0, moveSpeed, 0);
+		}
+		else if (kanan){
+			karakter.transform.position = karakter.transform.position 
+											+ new Vector3(moveSpeed, 0, 0);
+		}
+		else if (kiri){
+			karakter.transform.position = karakter.transform.position 
+											- new Vector3(moveSpeed, 0, 0);
+		}
+
+
+	}
+
+	private void OnMouseDown (){
+		if (idButton == 1){
+			atas = true;
+			anim.SetBool("KeAtas", true);
+		}
+		else if (idButton == 2){
+			bawah = true;
+			anim.SetBool("KeBawah", true);
+		}
+		else if (idButton == 3){
+			kanan = true;
+			anim.SetBool("KeKanan", true);
+		}
+		else if (idButton == 4){
+			kiri = true;
+			anim.SetBool("KeKiri", true);
+		}
+	}
+//=====================================================
+/*	
+	private void OnMouseDrag (){
+		
+	}
+*/
+//=======================================================
+	private void OnMouseUp (){
+		if (idButton == 1 || idButton == 2 || idButton == 3 || idButton == 4){
+			karakter.transform.position = karakter.transform.position;
+			anim.SetBool("KeAtas", false);
+			anim.SetBool("KeBawah", false);
+			anim.SetBool("KeKanan", false);
+			anim.SetBool("KeKiri", false);
+
+			atas = false;
+			bawah = false;
+			kanan = false;
+			kiri = false;
+		}
+	}
+}
---
+++ GameControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour {
+
+	public GameObject [] buah;
+	public GameObject karakter;
+	public Text Score;
+	public int ScoreValue;
+
+	// Use this for initialization
+	void Start () {
+		ScoreValue = 0; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Mathf.Abs(karakter.transform.position.x - buah[0].transform.position.x) <= 1 &&
+			Mathf.Abs(karakter.transform.position.y - buah[0].transform.position.y) <= 1){
+				ScoreValue = ScoreValue+1; 
+				buah[0].transform.position = new Vector3(15,0,0);
+				buah[0].SetActive(false);
+		}
+		if (Mathf.Abs(karakter.transform.position.x - buah[1].transform.position.x) <= 1 &&
+			Mathf.Abs(karakter.transform.position.y - buah[1].transform.position.y) <= 1){
+				ScoreValue = ScoreValue+1; 
+				buah[1].transform.position = new Vector3(15,0,0);
+				buah[1].SetActive(false);
+		}
+		if (Mathf.Abs(karakter.transform.position.x - buah[2].transform.position.x) <= 1 &&
+			Mathf.Abs(karakter.transform.position.y - buah[2].transform.position.y) <= 1){
+				ScoreValue = ScoreValue+1; 
+				buah[2].transform.position = new Vector3(15,0,0);
+				buah[2].SetActive(false);
+		}
+		Score.text = "Score: "+ ScoreValue;
+	}
+}
